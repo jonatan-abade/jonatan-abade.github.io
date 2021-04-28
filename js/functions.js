2339f8989df91c6a78dbafe2bb363e758486e2be
Jonatan Abade <jonatanabade@gmail.com>
2021-04-28 18:14:33 -0400

    Diminuição no tamanho da imagem e correção no menu modile

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+if (window.innerWidth < 768) {
+    document.querySelector(".navbar-fixed.black-main").style.display = "block"
+    document.querySelector(".navbar-fixed").style.display = "none"
+} else {
+    document.querySelector(".navbar-fixed.black-main").style.display = "none"
+    document.querySelector(".navbar-fixed.").style.display = "block"
+}
