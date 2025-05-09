1d73a199ac1579af6193eb506d18643fba727198
jonatan-abade <jonatanabade@gmail.com>
2025-05-08 20:19:34 -0400

    Refactor responsive navbar display logic for improved mobile experience

--- js/functions.js
+++ js/functions.js
@@ -3,5 +3,5 @@ if (window.innerWidth < 768) {
     document.querySelector(".navbar-fixed").style.display = "none"
 } else {
     document.querySelector(".navbar-fixed.black-main").style.display = "none"
-    document.querySelector(".navbar-fixed.").style.display = "block"
+    document.querySelector(".navbar-fixed").style.display = "block"
 }
